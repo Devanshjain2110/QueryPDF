3cbc89e938620257226769df3a31205f29af5a7d
Devanshjain2110 <92254216+Devanshjain2110@users.noreply.github.com>
2023-10-26 19:24:59 +0530

    Initial Commit

---
+++ src/app/components/MaxWidthWrapper.tsx
@@ -0,0 +1,13 @@
+import { cn } from "@/lib/utils"
+import { ReactNode } from "react"
+
+
+function MaxWidthWrapper({children, className} : {className?: string, children : ReactNode})  {
+  return (
+    <div className={cn("mx-auto w-full max-w-screen-xl px-2.5 md:px-[1.25rem]", className)}>
+      {children}
+    </div>
+  )
+}
+
+export default MaxWidthWrapper
