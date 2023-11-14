569862c44e8df5ccf135e39b55528780f0fecefd
Devanshjain2110 <92254216+Devanshjain2110@users.noreply.github.com>
2023-11-14 18:06:05 +0530

    Connected upload thing pdf to db and implemented uploading feature

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,10 +1,11 @@
+import { db } from "@/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 
 const f = createUploadthing();
 
 export const ourFileRouter = {
-  PDFUploader: f({ image: { maxFileSize: "4MB" } })
+  PDFUploader: f({ pdf: { maxFileSize: "4MB" } })
     .middleware(async ({ req }) => {
       const {getUser} = getKindeServerSession()
       const user = getUser()
@@ -12,7 +13,17 @@ export const ourFileRouter = {
       if(!user || !user.id) throw new Error('Unauthorized')
       return { userId : user.id };
     })
-    .onUploadComplete(async ({ metadata, file }) => {}),
+    .onUploadComplete(async ({ metadata, file }) => {
+      const createdFile = await db.file.create({
+        data : {
+          key:file.key,
+          name: file.name,
+          userId : metadata.userId,
+          url : `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`,
+          uploadStatus: "PROCESSING"
+        }
+      })
+    }),
 } satisfies FileRouter;
 
 export type OurFileRouter = typeof ourFileRouter;
--- src/app/components/uploadArea.tsx
+++ src/app/components/uploadArea.tsx
@@ -1,40 +1,74 @@
-'use client'
+"use client";
 import { useUploadThing } from "@/lib/uploadThing";
 import { useState } from "react";
 import Dropzone from "react-dropzone";
 import { AiOutlineCloud, AiOutlineFile } from "react-icons/ai";
+import { trpc } from "../_trpc/client";
+import { useRouter } from "next/navigation";
 
 function UploadArea() {
-    const [isUploading, setIsUploading] = useState<boolean>(true)
-    const [uploadProgress, setUploadProgress] = useState<number>(0)
-    const {startUpload }  = useUploadThing('PDFUploader')
-
-    const progresSimulation = () => {
-      setUploadProgress(0)
-      const interval = setInterval(() => {
-        setUploadProgress((prevProgress) => {if(prevProgress >= 95){
-          clearInterval(interval)
-          return prevProgress
+  const router = useRouter();
+  const [isUploading, setIsUploading] = useState<boolean>(false);
+  const [uploadProgress, setUploadProgress] = useState<number>(0);
+  const { startUpload } = useUploadThing("PDFUploader");
+  const { mutate: startPolling } = trpc.getFile.useMutation({
+    onSuccess: (file) => {
+      router.push(`/dashboard/${file.id}`);
+    },
+    retry: true,
+    retryDelay: 500,
+  });
+  const progresSimulation = () => {
+    setUploadProgress(0);
+    const interval = setInterval(() => {
+      setUploadProgress((prevProgress) => {
+        if (prevProgress >= 95) {
+          clearInterval(interval);
+          return prevProgress;
         }
-        return prevProgress + 5
-      })
-    }, 500)
-  return interval}
+        return prevProgress + 5;
+      });
+    }, 500);
+    return interval;
+  };
   return (
     <Dropzone
       multiple={false}
-      onDrop={ async (acceptedFile) => {
-      setIsUploading(true)
-      const progressInterval = progresSimulation()
-      await new Promise((resolve) => setTimeout(resolve, 10000))
+      onDrop={async (acceptedFile) => {
+        setIsUploading(true);
+        const progressInterval = progresSimulation();
 
-      const res = await startUpload(acceptedFile)
-      if(!res){
-        
-      }
-      
-      clearInterval(progressInterval)
-      setUploadProgress(100)
+        const res = await startUpload(acceptedFile);
+        if (!res) {
+          return (
+            <div className="toast toast-end top-10">
+              <div className="alert alert-info">
+                <span>New mail arrived.</span>
+              </div>
+              <div className="alert alert-success">
+                <span>Message sent successfully.</span>
+              </div>
+            </div>
+          );
+        }
+
+        const [fileResponse] = res;
+        const key = fileResponse.key;
+
+        if (!key) {
+          <div className="toast toast-end">
+            <div className="alert alert-info">
+              <span>New mail arrived.</span>
+            </div>
+            <div className="alert alert-success">
+              <span>Message sent successfully.</span>
+            </div>
+          </div>;
+        }
+
+        clearInterval(progressInterval);
+        setUploadProgress(100);
+        startPolling({key   })
       }}
     >
       {({ getRootProps, getInputProps, acceptedFiles }) => (
@@ -67,12 +101,39 @@ function UploadArea() {
                 </div>
               ) : null}
 
-              {
-                isUploading ? (<div className="flex items-center justify-center w-full mt-5 max-w-xs mx-auto">
-<progress className="progress progress-info w-56" value={uploadProgress} max="100"></progress>
-
-                </div>) : null
-              }
+              {isUploading ? (
+                <div className="flex flex-col items-center justify-center w-full mt-5 max-w-xs mx-auto">
+                  <progress
+                    className="progress progress-info w-56"
+                    value={uploadProgress}
+                    max="100"
+                  ></progress>
+                  {uploadProgress === 100 ? (
+                    <div className="flex gap-1 items-center justify-center text-sm text-zinc-700 text-center pt-2">
+                         <div role="status">
+                          <svg
+                            aria-hidden="true"
+                            className="w-4 h-4 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                            viewBox="0 0 100 101"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path
+                              d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                              fill="currentColor"
+                            />
+                            <path
+                              d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                              fill="currentFill"
+                            />
+                          </svg>
+                          <span className="sr-only">Redirecting...</span>
+                        </div>
+                    </div>
+                  ) : null}
+                </div>
+              ) : null}
+              <input type="file" className="hidden" id='dropzone-file' {...getInputProps()} />
             </label>
           </div>
         </div>
