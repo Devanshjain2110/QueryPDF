66f3d1ec4789e1ac6178d329365e36cfb690b7f6
Devanshjain2110 <92254216+Devanshjain2110@users.noreply.github.com>
2023-12-07 17:55:46 +0530

    Updated image input from path to imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 import MaxWidthWrapper from "./components/MaxWidthWrapper";
 import Link from "next/link";
 import { BsArrowRight } from "react-icons/bs";
+import preview from '../../public/preview.jpg'
+import dashImg from '../../public/dashboard.jpg'
 
 export default function Home() {
   return (
@@ -50,11 +52,12 @@ export default function Home() {
               <div className='mt-16 flow-root sm:mt-24'>
                 <div className='-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4'>
                   <Image
-                    src='/dashboard-preview.jpg'
+                    src={dashImg}
                     alt='product preview'
                     width={1364}
                     height={866}
-                    quality={100}
+                    quality={95}
+                    priority
                     className='rounded-md bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10'
                   />
                 </div>
@@ -149,11 +152,12 @@ export default function Home() {
           <div className='mt-16 flow-root sm:mt-24'>
             <div className='-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4'>
               <Image
-                src='/file-upload-preview.jpg'
+                src={preview}
                 alt='uploading preview'
                 width={1419}
                 height={732}
-                quality={100}
+                quality={95}
+                priority
                 className='rounded-md bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10'
               />
             </div>
